3bb13f05eadaf343734268b87e0c250a5dabb2f1
Prasanth <prasanthranger09@gmail.com>
2025-04-12 14:33:14 +0530

    update: removed the session logic

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 import { useState, useEffect } from "react";
@@ -23,11 +22,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <SessionProvider>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
-      </SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
       <ToasterWrapper />
     </QueryClientProvider>
   );
